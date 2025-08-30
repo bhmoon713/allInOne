2078c3e774fb4bde663fb6c68a4d0f25af913bce
bhmoon <bhmoon713@gmail.com>
2025-08-30 23:10:21 +0000

    Start from map, Camera added successfully

---
+++ main.js
@@ -0,0 +1,139 @@
+var app = new Vue({
+    el: '#app',
+
+    // computed values (camera
+    computed: {
+        ws_address: function() {
+            return `${this.rosbridge_address}`
+        },
+    },
+    // storing the state of the page
+    data: {
+        connected: false,
+        ros: null,
+        logs: [],
+        loading: false,
+        topic: null,
+        message: null,
+        rosbridge_address: '',
+        port: '9090',
+        mapViewer: null,
+        mapGridClient: null,
+        interval: null,
+    },
+    // helper methods to connect to ROS
+    methods: {
+        connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+                this.setCamera()            
+                this.mapViewer = new ROS2D.Viewer({
+                    divID: 'map',
+                    width: 420,
+                    height: 360
+                })
+
+                // Setup the map client.
+                this.mapGridClient = new ROS2D.OccupancyGridClient({
+                    ros: this.ros,
+                    rootObject: this.mapViewer.scene,
+                    continuous: true,
+                })
+                // Scale the canvas to fit to the map
+                this.mapGridClient.on('change', () => {
+                    this.mapViewer.scaleToDimensions(this.mapGridClient.currentGrid.width, this.mapGridClient.currentGrid.height);
+                    this.mapViewer.shift(this.mapGridClient.currentGrid.pose.position.x, this.mapGridClient.currentGrid.pose.position.y)
+                })
+
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+                document.getElementById('map').innerHTML = ''
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+
+        setTopic: function() {
+            this.topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/fastbot/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+        },
+        forward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.2, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        stop: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        backward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: -0.2, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnLeft: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.2, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnRight: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.2, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        setCamera: function() {
+            let without_wss = this.rosbridge_address.split('wss://')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+                divID: 'divCamera',
+                host: host,
+                width: 320,
+                height: 240,
+                topic: '/fastbot_1/camera/image_raw',
+                ssl: true,
+            })
+        },
+    },
+    mounted() {
+        this.interval = setInterval(() => {
+            if (this.ros != null && this.ros.isConnected) {
+                this.ros.getNodes((data) => { }, (error) => { })
+            }
+        }, 10000)
+    },
+})
