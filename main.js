d033fe3508954db58502e1bf849a71506f602821
bhmoon <bhmoon713@gmail.com>
2025-08-31 01:17:11 +0000

    joy stick added, now cam and map works

--- main.js
+++ main.js
@@ -15,11 +15,34 @@ var app = new Vue({
         loading: false,
         topic: null,
         message: null,
-        rosbridge_address: '',
+        rosbridge_address: 'wss://i-0c34017576fd20f71.robotigniteacademy.com/4f4f113a-01d0-43db-8f40-b6c1394290d7/rosbridge/',
         port: '9090',
         mapViewer: null,
         mapGridClient: null,
         interval: null,
+
+        // page content
+        menu_title: 'Connection',
+        // dragging data
+        dragging: false,
+        x: 'no',
+        y: 'no',
+        dragCircleStyle: {
+            margin: '0px',
+            top: '0px',
+            left: '0px',
+            display: 'none',
+            width: '75px',
+            height: '75px',
+        },
+        // joystick valules
+        joystick: {
+            vertical: 0,
+            horizontal: 0,
+        },
+        // publisher
+        pubInterval: null,
+
     },
     // helper methods to connect to ROS
     methods: {
@@ -30,6 +53,7 @@ var app = new Vue({
             })
             this.ros.on('connection', () => {
                 this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.pubInterval = setInterval(this.publish, 100)
                 this.connected = true
                 this.loading = false
                 this.setCamera()            
@@ -59,9 +83,25 @@ var app = new Vue({
                 this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
                 this.connected = false
                 this.loading = false
+                clearInterval(this.pubInterval)
                 document.getElementById('map').innerHTML = ''
             })
         },
+        
+        publish: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/fastbot_1/cmd_vel',
+                // messageType: 'geometry_msgs/Twist'
+                messageType: 'geometry_msgs/msg/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: this.joystick.horizontal, },
+            })
+            topic.publish(message)
+        },
+        
         disconnect: function() {
             this.ros.close()
         },
@@ -128,8 +168,61 @@ var app = new Vue({
                 ssl: true,
             })
         },
+        // joystick related
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/fastbot_1/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 0.2, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            topic.publish(message)
+        },
+        startDrag() {
+            this.dragging = true
+            this.x = this.y = 0
+        },
+        stopDrag() {
+            this.dragging = false
+            this.x = this.y = 'no'
+            this.dragCircleStyle.display = 'none'
+            this.resetJoystickVals()
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                this.x = event.offsetX
+                this.y = event.offsetY
+                let ref = document.getElementById('dragstartzone')
+                this.dragCircleStyle.display = 'inline-block'
+
+                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
+                let maxTop = minTop + 200
+                let top = this.y + minTop
+                this.dragCircleStyle.top = `${top}px`
+
+                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
+                let maxLeft = minLeft + 200
+                let left = this.x + minLeft
+                this.dragCircleStyle.left = `${left}px`
+
+                this.setJoystickVals()
+            }
+        },
+        setJoystickVals() {
+            this.joystick.vertical = -1 * ((this.y / 200) - 0.5)
+            this.joystick.horizontal = +1 * ((this.x / 200) - 0.5)
+        },
+        resetJoystickVals() {
+            this.joystick.vertical = 0
+            this.joystick.horizontal = 0
+        },
+
     },
     mounted() {
+        window.addEventListener('mouseup', this.stopDrag)
         this.interval = setInterval(() => {
             if (this.ros != null && this.ros.isConnected) {
                 this.ros.getNodes((data) => { }, (error) => { })
